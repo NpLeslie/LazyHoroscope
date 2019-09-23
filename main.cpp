e98f48bd290a5afd6ae760b2dce1d06bdf876522
npleslie <npleslie@mail.csuchico.edu>
2019-09-23 16:11:41 -0700

    collects the users first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Author:Nathaniel Leslie
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
+  string fname = "";
+  string lname = "";
 
   //get user input
-
+  cout<<"Please enter your first name: ";
+  cin>>fname;
+  cout<<"Please enter your last name: ";
+  cin>>lname;
+  cout<<"Welcome, "<<fname<<" "<<lname<<", here is your fortune..."<<endl;
   //tell fortune
 
   return 0;
